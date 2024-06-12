8aad2af885424f61e7608c716c80298e085f75ac
Srikanth1804 . <mrmadukkur1804@gmail.com>
2024-06-12 17:28:55 +0000

    .

--- react/src/Hooks/Useeffectdemo.jsx
+++ react/src/Hooks/Useeffectdemo.jsx
@@ -1,19 +1,34 @@
 import React, { useEffect, useState } from 'react'
 
 export const Useeffectdemo = () => {
-
-    let[Data,Setdata]=useState([])
-    useEffect=(()=>{
-        fetch('https://fakestoreapi.com/products/1')
-        .then(res=>res.json())
-        .then(json=>Setdata(json))
-    },[])
+  let [Data,SetData]=useState([])
+  useEffect(()=>{
+    fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=>SetData(json))
+  },[])
   return (
     <div>
-        {Data.map((File)=>{
-            <h1>{File.title}</h1>,
-            <img src={File.image} alt="" />
-        })}
+    {Data.map((Store)=>{
+      return(
+        <div className='card' style={{width:"300px",display:"inline-block",margin:"10px"}}>
+        <div className='card-header'>
+          <h5 className='card-title'>
+            {Store.title}
+          </h5>
+        </div>
+        <div className='card-body'>
+          <img src={Store.image} alt="" className='img-fluid'/>
+          <h6>Price:<span className='badge bg-secondary'>{Store.price}</span><span>   </span>
+          Category:<span className='badge bg-primary'>{Store.category}</span></h6>
+        </div>
+        <div>
+          <p className='lead'>{Store.description}</p>
+        </div>
+        
+        </div>
+      )
+    })}
     </div>
   )
 }
